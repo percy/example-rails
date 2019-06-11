736ac484d2265033e7294275820e34a177969dbd
Yusuke Goto <u.suke.goto@gmail.com>
2019-06-12 05:38:41 +0900

    fix migration version (#25)

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519181601) do
+ActiveRecord::Schema.define(version: 2015_05_19_181601) do
 
   create_table "todos", force: :cascade do |t|
-    t.string   "title"
-    t.boolean  "is_completed",    default: false
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
-    t.string   "session_user_id"
+    t.string "title"
+    t.boolean "is_completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "session_user_id"
   end
 
 end
